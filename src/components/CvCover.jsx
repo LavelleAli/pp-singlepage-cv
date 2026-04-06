4205364e8d7fd1ce4c276e855ad8f276f3738168
Lavelle_Ali <lavelleali7@gmail.com>
2026-04-06 19:11:03 -0400

    Update: 100%

--- src/components/CvCover.jsx
+++ src/components/CvCover.jsx
@@ -26,12 +26,14 @@ const Nav = () => {
           <h4 className="subtitle"> <span className="text__color--green">Skills</span> </h4>
           <p>HTML, CSS, JavaScript, React, Responsive Web Design, Technical Writing, Presentation</p>
         </div>
+
         <div className="education__section">
           <h4 className="subtitle__edu"> <span className="text__color--green">Education</span> </h4>
           <p className="text__color--blue">School Name, Location - Degree</p>
           <p className="para__height">Month 20xx to Month 20xx</p>
           <p>List of Accomplishments attained at establishments</p>
         </div>
+
         <div className="experience__section">
           <h4 className="subtitle__edu"> <span className="text__color--green">Experience</span> </h4>
           <p className="text__color--blue">Company Name, Location - Job Title</p>
@@ -42,8 +44,9 @@ const Nav = () => {
             <li>List of Achievements</li>
           </ul>
           <p>Skills: List of Skills used or gained at this conpany </p>
-
-          <p className="text__color--blue multi">Company Name, Location - Job Title</p>
+        </div>
+        <div className="experience__section section__pt2">
+          <p className="text__color--blue">Company Name, Location - Job Title</p>
           <p className="para__height">Month 20xx to Month 20xx</p>
           <ul className="loa1">
             <li>List of Achievements</li>
@@ -52,7 +55,7 @@ const Nav = () => {
           </ul>
           <p>Skills: List of Skills used or gained at this conpany </p>
         </div>
-        
+
         <div className="Across__section">
           <h4 className="subtitle"> <span className="text__color--green">Across the Internet</span> </h4>
           <p>Add your LinkedIn, GitHub profile links</p>
